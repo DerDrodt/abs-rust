0c8c8445a7608598a934ce34e53e34e537cdddbd
Daniel <d.drodt@gmx.de>
2020-10-31 16:53:51 +0100

    Remove rand gen

--- crates/abs_rand_module/README.md
+++
@@ -1,3 +0,0 @@
-# abs_rand_module
-
-A crate for generating random ABS modules. Mainly for testing performance of the ABS compiler
--- crates/abs_rand_module/src/gen/lit.rs
+++
@@ -1,5 +0,0 @@
-use abs_syntax::ast;
-
-pub fn create_lit<S: Into<String>>(s: S) -> ast::Literal {
-    ast::Literal { s: s.into() }
-}
